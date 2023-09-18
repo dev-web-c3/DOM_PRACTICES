25d6b1c2a6c4da769f55f044d99337b697572d07
Sacre-MBIKU-WAKANE <sacrembiku@mukwakani.com>
2023-09-18 18:19:21 +0100

    stock color palettes

---
+++ modal/constants.js
@@ -0,0 +1,48 @@
+const colorPalette = [
+  {
+    id: "Dark Gray",
+    colors: [
+      {
+        title: "Dark Gray / Black",
+        colorCode: "#1C1C1C",
+      },
+      {
+        title: "Dark Gray / Dark -1",
+        colorCode: "#0A0A0A",
+      },
+      {
+        title: "Dark Gray / Dark -2",
+        colorCode: "#0E0E0E",
+      },
+      {
+        title: "Dark Gray / Dark -3",
+        colorCode: "#161616",
+      },
+      {
+        title: "Dark Gray / Dark -4",
+        colorCode: "#2B2B2B",
+      },
+      {
+        title: "Dark Gray / Dark -5",
+        colorCode: "#404040",
+      },
+    ],
+  },
+  {
+    id: "Gray",
+    colors: [
+      {
+        title: "Gray / White",
+        colorCode: "#ffffff",
+      },
+      {
+        title: "Gray / Light",
+        colorCode: "#CDCDCD",
+      },
+      {
+        title: "Gray / Medium",
+        colorCode: "#5F5F5F",
+      },
+    ],
+  },
+];
